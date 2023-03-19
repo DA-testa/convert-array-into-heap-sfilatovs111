36cf1df43b4ddc85c54eebbf92dad99fb8336968
Sergejs 221RDB111 <125831670+sfilatovs111@users.noreply.github.com>
2023-03-19 23:36:35 +0200

    v1

--- build_heap.py
+++ build_heap.py
@@ -1,41 +1,43 @@
-# python3
+#python3
 
-
-def build_heap(data):
+def build_heap(arr):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    n = len(arr)
+
+
+
+    def sift_down(i):
+        min_index = i
+        left_child = 2 * i + 1
+        if left_child < n and arr[left_child] < arr[min_index]:
+            min_index = left_child
+        right_child = 2 * i + 2
+        if right_child < n and arr[right_child] < arr[min_index]:
+            min_index = right_child
+        if i != min_index:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+            swaps.append((i, min_index))
+            sift_down(min_index)
+    for i in range(n // 2, -1, -1):
+        sift_down(i)
+
+
 
 
     return swaps
 
 
+
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
     n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
+    arr = list(map(int, input().split()))
+    assert len(arr) == n
+    swaps = build_heap(arr)
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
+
+
 
 
 if __name__ == "__main__":
