55580311d10f0b994fa531321b5bc103ce900234
Sergejs 221RDB111 <125831670+sfilatovs111@users.noreply.github.com>
2023-05-26 09:26:43 +0300

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,44 +1,44 @@
-#python3
+import os
 
-def build_heap(arr):
-    swaps = []
-    n = len(arr)
+def build_heap(data):
+heap_changes = []
+size = len(data)
+for i in range(size // 2, -1, -1):
+heapify(data, i, heap_changes)
+return heap_changes
 
-
-
-    def sift_down(i):
-        min_index = i
-        left_child = 2 * i + 1
-        if left_child < n and arr[left_child] < arr[min_index]:
-            min_index = left_child
-        right_child = 2 * i + 2
-        if right_child < n and arr[right_child] < arr[min_index]:
-            min_index = right_child
-        if i != min_index:
-            arr[i], arr[min_index] = arr[min_index], arr[i]
-            swaps.append((i, min_index))
-            sift_down(min_index)
-    for i in range(n // 2, -1, -1):
-        sift_down(i)
-
-
-
-
-    return swaps
-
-
+def heapify(data, i, heap_changes):
+size = len(data)
+min_index = i
+left = 2 * i + 1
+right = 2 * i + 2
+if left < size and data[left] < data[min_index]:
+min_index = left
+if right < size and data[right] < data[min_index]:
+min_index = right
+if min_index != i:
+heap_changes.append((i, min_index))
+data[i], data[min_index] = data[min_index], data[i]
+heapify(data, min_index, heap_changes)
 
 def main():
-    n = int(input().strip())
-    data = list(map(int, input().strip().split()))
-    assert len(data) == n
+user_input = input()
+if "I" in user_input:
+n = int(input())
+data = list(map(int, input().split()))
+assert len(data) == n
+elif "F" in user_input:
+file_name = input()
+directory = './tests/'
+file_path = os.path.join(directory, file_name)
+with open(file_path, mode="r") as file:
+n = int(file.readline())
+data = list(map(int, file.readline().split()))
+heap_changes = build_heap(data)
+print(len(heap_changes))
+for i, j in heap_changes:
+print(i, j)
 
-    swaps = build_heap(data)
-
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-
-
-if __name__ == '__main__':
-    main()
+if name == "main":
+main()
+ 
